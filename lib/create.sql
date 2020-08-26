d66ce513f89294223b5d17a54e23ca303a906c04
Trevis McGill <trevismcgill25@gmail.com>
2020-08-26 13:43:58 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,12 +1,18 @@
 CREATE TABLE users (
 id INT PRIMARY KEY,
-
+name TEXT,
+age INT
 );
 
 CREATE TABLE projects (
-
+id INT PRIMARY KEY,
+title TEXT,
+category TEXT,
+funding_goal REAL,
+start_date TEXT,
+end_date TEXT
 );
 
 CREATE TABLE pledges (
-
+id INT PRIMARY KEY,
 );
